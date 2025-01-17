fb081d597e7a61e3543570c20a526f2bbc0c3d9b
Henry22Chen <chencyl10@outlook.com>
2025-01-17 16:32:18 +0800

    Samples

--- Packages/com.henrychen.component-bind-tool/Samples~/Basic/Scripts/AutoBindTest.cs
+++ Packages/com.henrychen.component-bind-tool/Samples~/Basic/Scripts/AutoBindTest.cs
@@ -1,23 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using ComponentBind;
 using UnityEngine;
+using UnityEngine.UI;
 
-public partial class AutoBindTest : MonoBehaviour
+namespace ComponentBindExample
 {
-
-    private int count = 0;
-
-
-    void Start()
+    public partial class AutoBindTest : AutoBindBehaviour, IAutoBindPage
     {
-        GetBindComponents(gameObject);
-        m_Btn_Test2.onClick.AddListener(OnBtnClick);
+        private int _count = 0;
+        private Image currentImage;
+        
+        private Color _highlight = Color.green;
+        
+        private void Start()
+        {
+            _btnTest2.onClick.AddListener(OnBtnClick);
+        }
+
+        private void OnBtnClick()
+        {
+            _count++;
+            _txtTest3.text = "点击了按钮" + _count + "次";
+            _imgTest1.gameObject.SetActive(!_imgTest1.gameObject.activeInHierarchy);
+            _testCompTest2.Print();
+
+            var idx = _count % _imgImagesList.Count;
+            if (currentImage != null)
+            {
+                currentImage.color = Color.white;
+            }
+            currentImage = _imgImagesList[idx];
+            currentImage.color = _highlight;
+        }
     }
 
-    private void OnBtnClick()
-    {
-        count++;
-        m_Txt_Test3.text = "点击了按钮" + count + "次";
-        m_Img_Test1.gameObject.SetActive(!m_Img_Test1.gameObject.activeInHierarchy);
-    }
 }
