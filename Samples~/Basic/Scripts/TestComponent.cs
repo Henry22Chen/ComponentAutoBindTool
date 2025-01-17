26178d30b634ca54431dcbd0f53db8777b6bbdc9
Henry22Chen <chencyl10@outlook.com>
2025-01-17 16:32:18 +0800

    Samples

---
+++ Samples~/Basic/Scripts/TestComponent.cs
@@ -0,0 +1,14 @@
+﻿using ComponentBind;
+using UnityEngine;
+
+namespace ComponentBindExample
+{
+    [AutoBindable("TestComp")]
+    public class TestComponent : MonoBehaviour
+    {
+        public void Print()
+        {
+            Debug.Log("TestComponent Print");
+        }
+    }
+}
